777241c5ee39ee1aa7d0397b1523404fd5ae362e
Suraj Thapa <stsurajst@gmail.com>
2019-11-22 13:26:19 +0545

    Updates

---
+++ precache-manifest.7cd9dde909aeb22430a4ce6da3f86b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2c5ec1c41a67aea3724e09739d9366",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6b9a2fd47ca2f57b8b57",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7d38b5e95589302c7092",
+    "url": "/contactmanager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b9a2fd47ca2f57b8b57",
+    "url": "/contactmanager/static/js/2.bce16817.chunk.js"
+  },
+  {
+    "revision": "7d38b5e95589302c7092",
+    "url": "/contactmanager/static/js/main.3b926dbd.chunk.js"
+  },
+  {
+    "revision": "c040dbd8586cd548482f",
+    "url": "/contactmanager/static/js/runtime~main.f3662a3d.js"
+  }
+]);
